7e48520e38a47faee004ecf87c88d693af467e6d
Niv Mimran <41971075+nmimran99@users.noreply.github.com>
2023-01-09 12:02:17 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		MONGODB_URI:
-			"mongodb+srv://xfhadmin:<password>@cluster0.f3yre.mongodb.net/xfh-db?retryWrites=true&w=majority",
-		SENDGRID_API_KEY:
-			"<key>",
-	},
-};
